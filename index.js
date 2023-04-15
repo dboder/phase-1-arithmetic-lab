f88ae94f7be31f9ea48767125892f1f15861c28f
Dorsey B <dorseybec@gmail.com>
2023-04-15 12:45:37 -0700

    compelte

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+// 1
+let num1 = 31;
+let num2 = 2;
+let num3 = 48;
+let num4 = 11;
+let multiply = num1 * num2;
+// 2
+let random = Math.floor(Math.random() * 10) + 1;
+// 3
+let mod =  num3  % num4;
+// 4
+let max = Math.max(1,4,5,20);
